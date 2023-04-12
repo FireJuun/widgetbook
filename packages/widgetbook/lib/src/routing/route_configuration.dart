337175f5c6914656d10df0b4b39cb88b7f370bc1
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2023-04-12 16:21:50 +0200

    fix panels

--- packages/widgetbook/lib/src/routing/route_configuration.dart
+++ packages/widgetbook/lib/src/routing/route_configuration.dart
@@ -1,12 +1,35 @@
+import '../state/state.dart';
+
 class RouteConfiguration {
-  const RouteConfiguration({
+  RouteConfiguration({
     required this.location,
-  });
+  }) : queryParameters = Uri.parse(location).queryParameters;
 
   final String location;
+  final Map<String, String> queryParameters;
 
   Map<String, String> get queryParams {
     final uri = Uri.parse(location);
     return uri.queryParameters;
+  }
+
+  String get path => queryParameters['path'] ?? '';
+
+  Set<WidgetbookPanel> get panels {
+    final parameter = queryParameters['panels'];
+
+    if (parameter == null) {
+      return WidgetbookPanel.values.toSet();
+    }
+
+    if (parameter.isEmpty || parameter == '{}') {
+      return {};
+    }
+
+    return parameter
+        .replaceAll(RegExp('[{}]'), '')
+        .split(',')
+        .map((name) => WidgetbookPanel.values.byName(name))
+        .toSet();
   }
 }
--- packages/widgetbook/lib/src/routing/router_delegate.dart
+++ packages/widgetbook/lib/src/routing/router_delegate.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:widgetbook/src/widgetbook_shell.dart';
-import 'package:widgetbook/src/workbench/workbench.dart';
 
 import '../../widgetbook.dart';
 import '../state/state.dart';
+import '../widgetbook_shell.dart';
+import '../workbench/workbench.dart';
 import 'route_configuration.dart';
 import 'router_delegate_base.dart';
 
@@ -34,15 +34,12 @@ class RouterDelegate extends RouterDelegateBase {
         MaterialPage(
           child: WidgetbookScope(
             state: WidgetbookState(
-              path: _configuration.queryParams['path'] ?? '',
-              panels: _configuration.queryParams['panels'] == null
-                  ? WidgetbookPanel.values.toSet()
-                  : WidgetbookPanelParser.parse(
-                      _configuration.queryParams['panels']!,
-                    ),
+              path: _configuration.path,
+              panels: _configuration.panels,
               queryParams: {
+                 // Copy from UnmodifiableMap
                 ..._configuration.queryParams
-              }, // Copy from UnmodifiableMap
+              },
               addons: addons,
               appBuilder: appBuilder,
               catalog: catalog,
@@ -50,7 +47,7 @@ class RouterDelegate extends RouterDelegateBase {
             child: WidgetbookShell(
               child: Workbench(),
             ),
-          ),
+
         ),
       ],
     );
